aed54df92a760e05fc495d9c0dfd2bdeb6d2adf2
DuNGEOnmassster <normanzheng6606@gmail.com>
2024-12-18 09:20:19 +0000

    update download weights shell scripts

---
+++ download_weights.sh
@@ -0,0 +1,6 @@
+huggingface-cli download --resume-download Junjie96/UniPortrait --local-dir models
+huggingface-cli download --resume-download h94/IP-Adapter --local-dir models/IP-Adapter
+
+huggingface-cli download --resume-download SG161222/Realistic_Vision_V5.1_noVAE --local-dir models/SG161222/Realistic_Vision_V5.1_noVAE
+huggingface-cli download --resume-download stabilityai/sd-vae-ft-mse --local-dir models/stabilityai/sd-vae-ft-mse
+huggingface-cli download --resume-download lllyasviel/control_v11p_sd15_openpose --local-dir models/lllyasviel/control_v11p_sd15_openpose
